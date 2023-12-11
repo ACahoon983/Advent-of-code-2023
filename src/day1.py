59b6ab1f249bacdc7999596351324c46d4593e8c
Andrew Cahoon <acahoon983@gmail.com>
2023-12-11 17:12:17 -0500

    Day 1 solved

--- src/day1.py
+++ src/day1.py
@@ -1,18 +1,57 @@
+def convertString(line):
+    line = line.replace("one", "o1ne")
+    line = line.replace("two", "t2wo")
+    line = line.replace("three", "t3hree")
+    line = line.replace("four", "f4our")
+    line = line.replace("five", "f5ive")
+    line = line.replace("six", "s6ix")
+    line = line.replace("seven", "s7even")
+    line = line.replace("eight", "e8ight")
+    line = line.replace("nine", "n9ine")
+    return line
+
 def part1(filename):
     file = open(filename, 'r')
     calibrationStrings = file.readlines()
     calibrationSum = 0
-    value1 = None
+    value1 = 0
     value2 = 0
     for calibrationLine in calibrationStrings:
+        value1 = None
         for character in calibrationLine:
-            if value1 == None:
+            if value1 is None:
                 if character.isdigit():
-                  value1 = int(character)
+                    value1 = int(character)
+                    value2 = value1
             else:
                 if character.isdigit():
                     value2 = int(character)
         calibrationSum += value1 * 10 + value2
+    file.close
     return calibrationSum
 
-        
+def part2(filename):
+    file = open(filename, 'r')
+    calibrationStrings = file.readlines()
+    calibrationSum = 0
+    value1 = 0
+    value2 = 0
+    for calibrationLine in calibrationStrings:
+        convertedLine = convertString(calibrationLine)
+        value1 = None
+        for character in convertedLine:
+            if value1 is None:
+                if character.isdigit():
+                    value1 = int(character)
+                    value2 = value1
+            else:
+                if character.isdigit():
+                    value2 = int(character)
+        calibrationSum += value1 * 10 + value2
+    file.close
+    return calibrationSum
+
+
+if __name__ == '__main__':
+    print(f'part 1 answer is {part1("input/day1.txt")}')
+    print(f'part 2 answer is {part2("input/day1.txt")}')
--- test/test_day1.py
+++
@@ -1,5 +0,0 @@
-from day1 import part1
-
-
-def test_part1():
-    assert part1('input/day1example.txt') == 142
